9cd8ae1cc264220950f3218dbdb8ffa02ff55877
zhongcanyu <zcyzcy159162>
2023-10-20 14:37:22 +0800

    init

---
+++ src/test/java/com/example/yu/framework/YuFrameWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yu.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuFrameWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
