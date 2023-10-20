2a5b4afabc9381b2797cf6efb7b1749d2b2f725c
zhongcanyu <zcyzcy159162>
2023-10-20 16:37:45 +0800

    fix and feat

---
+++ src/main/java/com/example/yu/framework/constant/ConfigConstant.java
@@ -0,0 +1,18 @@
+package com.example.yu.framework.constant;
+
+/**
+ * Provides related configuration item constants
+ */
+public class ConfigConstant {
+
+    private final static String CONFIG_FILE = "yu.properties";
+    
+    private final static String JDBC_DRIVER = "yu.framework.jdbc.driver";
+    private final static String JDBC_URL = "yu.framework.jdbc.url";
+    private final static String JDBC_USERNAME = "yu.framework.jdbc.username";
+    private final static String JDBC_PASSWORD = "yu.framework.jdbc.password";
+    
+    private final static String APP_BASE_PACKAGE = "yu.framework.app.base_package";
+    private final static String APP_JSP_PATH = "yu.framework.app.jsp_path";
+    private final static String APP_ASSET_PATH = "yu.framework.app.asset_path";
+}
---
+++ src/main/java/com/example/yu/framework/helper/ConfigHelper.java
@@ -0,0 +1,4 @@
+package com.example.yu.framework.helper;
+
+public class ConfigHelper {
+}
