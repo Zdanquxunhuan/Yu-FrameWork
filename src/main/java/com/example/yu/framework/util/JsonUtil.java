c32408e770cea444eba979e9ad169d0b6efda5d9
ZCY <982287281@qq.com>
2023-10-21 19:08:24 +0800

    fix: log dependencies

--- src/main/java/com/example/yu/framework/util/JsonUtil.java
+++ src/main/java/com/example/yu/framework/util/JsonUtil.java
@@ -1,8 +1,9 @@
 package com.example.yu.framework.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
--- src/main/java/com/example/yu/framework/util/ReflectionUtil.java
+++ src/main/java/com/example/yu/framework/util/ReflectionUtil.java
@@ -1,10 +1,10 @@
 package com.example.yu.framework.util;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Reflection tool class
