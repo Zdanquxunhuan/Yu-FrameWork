e4397a12bba83424d69ebca9914ab59fa0713ddb
ZCY <982287281@qq.com>
2023-10-28 11:39:29 +0800

    fix: some additions

--- src/main/java/com/example/yu/framework/servlet/DispatcherServlet.java
+++ src/main/java/com/example/yu/framework/servlet/DispatcherServlet.java
@@ -1,10 +1,14 @@
 package com.example.yu.framework.servlet;
 
+import com.example.yu.framework.bean.Data;
 import com.example.yu.framework.bean.Handler;
+import com.example.yu.framework.bean.Param;
+import com.example.yu.framework.bean.View;
 import com.example.yu.framework.helper.BeanHelper;
 import com.example.yu.framework.helper.ConfigHelper;
 import com.example.yu.framework.helper.ControllerHelper;
 import com.example.yu.framework.init.ProgramInit;
+import com.example.yu.framework.util.*;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -15,6 +19,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.lang.reflect.Method;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
@@ -82,6 +88,8 @@ public class DispatcherServlet extends HttpServlet {
 
             //get and create request Params
             Map<String, Object> paramMap = new HashMap<String, Object>();
+
+            //POST?
             Enumeration<String> paramNames = request.getParameterNames();
             while (paramNames.hasMoreElements()) {
                 String paramName = paramNames.nextElement();
@@ -89,7 +97,56 @@ public class DispatcherServlet extends HttpServlet {
                 paramMap.put(paramName,paramValue);
             }
 
+            //GET?
+            String body = CodecUtil.decodeURL(StreamUtil.getString(request.getInputStream()));
+            if(StringUtil.isNotEmpty(body)){
+                String[] params = StringUtil.splitString(body, "&");
+                if (ArrayUtil.isNotEmpty(params)) {
+                    for (String param : params) {
+                        String[] array = StringUtil.splitString(param, "=");
+                        if (ArrayUtil.isNotEmpty(array) && array.length==2){
+                            String paramName = array[0];
+                            String paramValue = array[1];
+                            paramMap.put(paramName, paramValue);
+                        }
+                    }
+                }
+            }
+            Param param = new Param(paramMap);
 
+            Method actionMethod = handler.getYuAutowiredMethod();
+            Object result = ReflectionUtil.invokeMethod(controllerBean, actionMethod, param);
+
+            // 处理 Action 方法返回值
+            if (result instanceof View) {
+                //
+                View view = (View) result;
+                String path = view.getPath();
+                if (StringUtil.isNotEmpty(path)) {
+                    if (path.startsWith("/")) {
+                        response.sendRedirect(request.getContextPath() + path);
+                    } else {
+                        Map<String, Object> model = view.getModel();
+                        for (Map.Entry<String, Object> entry : model.entrySet()) {
+                            request.setAttribute(entry.getKey(), entry.getValue());
+                        }
+                        request.getRequestDispatcher(ConfigHelper.getAppJspPath() + path).forward(request, response);
+                    }
+                }
+            } else if (result instanceof Data) {
+                //
+                Data data = (Data) result;
+                Object model = data.getModel();
+                if (model != null) {
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+                    PrintWriter writer = response.getWriter();
+                    String json = JsonUtil.toJson(model);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            }
         }
     }
 }
